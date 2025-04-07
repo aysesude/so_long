0da4ee4ee87ba0eab9f513a81595127a0565dab2
aysesude <camiaysesude@gmail.com>
2025-04-07 08:12:06 +0000

    error printf change with write

--- error.c
+++ error.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   error.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aycami <aycami@student.42.fr>              +#+  +:+       +#+        */
+/*   By: aycami" <aycami@student.42.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 01:06:02 by aycami            #+#    #+#             */
-/*   Updated: 2025/04/07 01:35:15 by aycami           ###   ########.fr       */
+/*   Updated: 2025/04/07 08:04:51 by aycami"          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,17 +41,17 @@ void	free_map(t_so_long *func)
 void	ft_print_errors(t_so_long *func)
 {
 	if (func->char_err == 1)
-		printf("There is character rather than C 0 1 E P\n");
+		write(2, "There is character rather than C 0 1 E P\n", 41);
 	if (func->exit_err == 1 || func->exit_err == 2)
-		printf("There is no exit/ Too much exit point\n");
+		write(2, "There is no exit/ Too much exit point\n", 38);
 	if (func->start_err == 1 || func->start_err == 2)
-		printf("There is no player/ Too much player\n");
+		write(2, "There is no player/ Too much player\n", 36);
 	if (func->coll_err == 1)
-		printf("There is no collectible\n");
+		write(2, "There is no collectible\n", 24);
 	if (func->path_err == 1 || func->exit_err == 1 || func->start_err == 1)
-		printf("There is no valid path\n");
+		write(2, "There is no valid path\n", 23);
 	if (func->wall_err == 1)
-		printf("Walls are missing on the sides\n");
+		write(2, "Walls are missing on the sides\n", 31);
 	if (func->exit_err == 1 || func->exit_err == 2 || func->start_err == 1
 		|| func->start_err == 2 || func->coll_err == 1 || func->path_err == 1
 		|| func->wall_err == 1 || func->char_err == 1)
@@ -66,6 +66,7 @@ void	ft_error_map_game_exit(t_so_long *func, int i)
 
 void	ft_error(char *msg)
 {
-	printf("%s\n", msg);
+	write(2, msg, ft_strlen(msg));
+	write(2, "\n", 1);
 	exit(1);
 }
